befffc0dc9e18815dadac7f0f63f5a6214b46ffb
tomz87 <tom.zlotikman@gmail.com>
2026-01-22 15:17:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TOKEN = os.environ.get("8518103041:AAGwbs3RfKSZRly39cNH-pXEpKlvDAhYW1A")
+GROUP_ID = int(os.environ.get("-1003587001321", "0"))
 API_URL = f"https://api.telegram.org/bot{TOKEN}"
 
 @app.get("/")
@@ -20,6 +21,8 @@ def webhook():
     if "message" in update and "text" in update["message"]:
         chat_id = update["message"]["chat"]["id"]
         text = update["message"]["text"]
+        is_group = (chat_id == GROUP_ID)
+
 
         # תגובת בדיקה
         if text.lower() == "שלום":
@@ -30,5 +33,11 @@ def webhook():
                     "text": "היי 👋 הבוט מחובר ועובד!"
                 }
             )
+        if text == "/ping" and is_group:
+            requests.post(
+                f"{API_URL}/sendMessage",
+                json={"chat_id": chat_id, "text": "🏓 pong (הבוט חי בקבוצה)"},
+            )
+
 
     return "OK", 200
